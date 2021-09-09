dc4f6e9f9db2a3ec6cc736ecfe1667d1a6fbfb4a
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-09-09 16:29:42 -0300

    Profile missions

--- src/components/missionsList.js
+++ src/components/missionsList.js
@@ -7,12 +7,6 @@ const Missions = () => {
   const missionsList = useSelector((state) => state.missionsReducer.missions);
   const dispatch = useDispatch();
 
-  useEffect(async () => {
-    if (!missionsList.length) {
-      await dispatch(setMission());
-    }
-  }, []);
-
   const reserveHandler = (e) => {
     const payload = e.target.parentNode.parentNode.id;
     dispatch(missionReserve(payload));
@@ -23,35 +17,43 @@ const Missions = () => {
     dispatch(missionCancel(payload));
   };
 
+  useEffect(() => {
+    if (!missionsList.length) {
+      dispatch(setMission());
+    }
+  }, []);
+
   return (
     <div>
       <table>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th colSpan="2">Status</th>
-        </tr>
-        {(missionsList.map((mission) => (
-          <tr key={mission.id} id={mission.id}>
-            <td className="mission_name">{mission.name}</td>
-            <td className="description">{mission.description}</td>
-            <td className="member_cnt">
-              {
-                mission.reserved
-                  ? <span className="active_member">Active member</span>
-                  : <span className="inactive_member">NOT A MEMBER</span>
-              }
-
-            </td>
-            <td className="btn_ctn">
-              {
-                mission.reserved
-                  ? <button type="button" className="cancel_btn" onClick={cancelHandler}>Leave Mission</button>
-                  : <button type="button" className="reserve_btn" onClick={reserveHandler}>Join Mission</button>
-              }
-            </td>
+        <tbody>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th colSpan="2">Status</th>
           </tr>
-        )))}
+          {(missionsList.map((mission) => (
+            <tr key={mission.id} id={mission.id}>
+              <td className="mission_name">{mission.name}</td>
+              <td className="description">{mission.description}</td>
+              <td className="member_cnt">
+                {
+                  mission.reserved
+                    ? <span className="active_member">Active member</span>
+                    : <span className="inactive_member">NOT A MEMBER</span>
+                }
+
+              </td>
+              <td className="btn_ctn">
+                {
+                  mission.reserved
+                    ? <button type="button" className="cancel_btn" onClick={cancelHandler}>Leave Mission</button>
+                    : <button type="button" className="reserve_btn" onClick={reserveHandler}>Join Mission</button>
+                }
+              </td>
+            </tr>
+          )))}
+        </tbody>
       </table>
     </div>
   );
